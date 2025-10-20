9ab96d5b751dd807ac4dd9243226805e59fdf8d1
Thais Rodrigues <thaiss.rodriguess@icloud.com>
2025-10-20 16:56:45 -0300

    criando cenarios

--- cypress/e2e/buscar-dispositivo.cy.js
+++ cypress/e2e/buscar-dispositivo.cy.js
@@ -5,16 +5,45 @@ describe('Buscar Dispositivo', () => {
         cy.request({
             method: 'GET',
             url: 'https://api.restful-api.dev/objects/3'
-        })
-    })
-
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            //expect(response.body.data.capacityGB).to.eq(512);
+            expect(response.body.data.color).to.eq('Cloudy White');
+            expect(response.body.id).to.eq('3');
+            expect(response.body.name).to.eq('Apple iPhone 12 Pro Max');
+            
+        }) 
+    });
     it('Buscar dispositivo inexistente', () => {
         cy.request({
             method: 'GET',
             url: 'https://api.restful-api.dev/objects/3XPTO',
             failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(404);
+            expect(response.body.error).to.eq('Oject with id=3XPTO was not found.');
         })
-
-    })
-}
-)
+    });
+    it.only('Cadastrar dispositivo', () =>{
+        cy.request({
+            method: 'POST',
+            url: 'https://api.restful-api.dev/objects',
+            body: {
+                "name": "Samsung Galaxy S21 Ultra",
+                "data": {
+                    "year": 2024,
+                    "price": 6000,
+                    "CPU model": "Samsung Exynos 2100",
+                    "Hard disk size": "1 TB",
+                    "color": "red"
+                }
+            }
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.name).to.eq('Samsung Galaxy S21 Ultra');
+            expect(response.body.data.year).to.eq(2024);
+            expect(response.body.data.price).to.eq(6000);
+            expect(response.body.data.color).to.eq('red');
+        })
+    });
+});
